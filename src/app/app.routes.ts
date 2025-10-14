4345106127af246e6b580d6fe3a5ea91f8807dd6
Gabriel Fagundes <gafagundesmesquita@gmail.com>
2025-10-14 19:18:53 -0300

    feat: configure routing for all pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Characters } from './pages/characters/characters';
+import { TailedBeasts } from './pages/tailed-beasts/tailed-beasts';
+import { Clans } from './pages/clans/clans';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'characters',
+    component: Characters
+  },
+  {
+    path: 'tailed-beasts',
+    component: TailedBeasts
+  },
+  {
+    path: 'clans',
+    component: Clans
+  }
+];
